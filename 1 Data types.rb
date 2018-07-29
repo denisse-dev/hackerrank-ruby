782982eb83d4cec86be75b648b99c5f297a79fe5
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-07-29 15:30:22 -0500

    Day 1: Data types

---
+++ 1 Data types.rb
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+si = 0
+sd = 0.0
+ss = ''
+
+# Read and save an integer, double, and String to your variables.
+si = gets.to_i
+sd = gets.to_f
+ss = gets
+
+# Print the sum of both integer variables on a new line.
+puts i + si
+
+# Print the sum of the double variables on a new line.
+puts d + sd
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + ss
