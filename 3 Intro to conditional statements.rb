018cd281c4fcd6aa78ee67afa48fee4a2551b691
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-07-31 16:27:56 -0500

    Day 3: Conditional statements

---
+++ 3 Intro to conditional statements.rb
@@ -0,0 +1,16 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+N = gets.to_i
+
+if N % 2 != 0
+  puts "Weird"
+elsif N >= 2 && N <= 5
+  puts "Not Weird"
+elsif N >= 6 && N <= 20
+  puts "Weird"
+else
+  puts "Not Weird"
+end
